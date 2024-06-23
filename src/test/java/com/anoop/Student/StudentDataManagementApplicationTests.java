de23ea4e805d8ca049335ee83054785e7a07111a
unknown <anoopabc139@gmail.com>
2024-06-23 19:46:18 +0530

    Initial Commit

---
+++ src/test/java/com/anoop/Student/StudentDataManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anoop.Student;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentDataManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
